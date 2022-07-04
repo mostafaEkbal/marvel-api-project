aedb3b1a6c9f534b4fd471865177aecce79a1976
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-07-04 21:44:12 +0200

    final setups (i hope)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 let baseURL = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f5ae36bdac41720b22fe45a619940cd7&hash=a473899226e72fc5a0138c0edf48520f&limit=100';
+let container = document.querySelector('.container');
+let num = 1;
 let characterNames = [
 
 ]
@@ -11,11 +13,19 @@ for (let i = 0; i < 16; i++) {
   .then((data) => { 
     let results = data.data.results;
     results.forEach(element => {
-      characterNames.push(element.name)
+      characterNames.push(element.name);
+      let nameContainer = document.createElement('div');
+      let nameNum = document.createElement('span');
+      let thename = document.createElement('span');
+      nameNum.append(`${num}  `);
+      thename.append(element.name);
+      nameContainer.append(nameNum);
+      nameContainer.append(thename);
+      container.append(nameContainer);
+      num++
     });
   });
 }
 
 
 
-console.log(characterNames.sort());
