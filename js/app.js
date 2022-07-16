142306f76d609c5bec61329520684c3f6b85a309
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-07-16 21:19:42 +0200

    beginning the project

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
-let baseURL = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=f5ae36bdac41720b22fe45a619940cd7&hash=a473899226e72fc5a0138c0edf48520f&limit=100';
+let baseURL = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${input}&orderBy=name&ts=1&apikey=f5ae36bdac41720b22fe45a619940cd7&hash=a473899226e72fc5a0138c0edf48520f&limit=100`;
 let container = document.querySelector('.container');
 let num = 1;
 let characterNames = [
 
 ]
 
-for (let i = 0; i < 16; i++) {
-  fetch(baseURL + `&offset=${i * 100}`)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => { 
-    let results = data.data.results;
-    results.forEach(element => {
-      characterNames.push(element.name);
-      let nameContainer = document.createElement('div');
-      let nameNum = document.createElement('span');
-      let thename = document.createElement('span');
-      nameNum.append(`${num}  `);
-      thename.append(element.name);
-      nameContainer.append(nameNum);
-      nameContainer.append(thename);
-      container.append(nameContainer);
-      num++
-    });
-  });
-}
+// for (let i = 0; i < 16; i++) {
+//   fetch(baseURL + `&offset=${i * 100}`)
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => { 
+//     let results = data.data.results;
+//     results.forEach(element => {
+//       characterNames.push(element.name);
+//       let nameContainer = document.createElement('div');
+//       let nameNum = document.createElement('span');
+//       let thename = document.createElement('span');
+//       let description = document.createElement('p');
+//       description.append(element.description)
+//       nameNum.append(`${num}  `);
+//       thename.append(element.name);
+//       nameContainer.append(nameNum);
+//       nameContainer.append(thename,description);
+//       container.append(nameContainer);
+//       num++
+//     });
+//   });
+// }
 
 
 
