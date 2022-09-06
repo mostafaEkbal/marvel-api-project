2fd2edb020e1d91fe91e8e1a3e1ef0fab6676773
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-09-06 22:19:00 +0200

    using params to create dynamic character data

--- marvel-app/src/components/CharacterDetails.jsx
+++ marvel-app/src/components/CharacterDetails.jsx
@@ -1,38 +1,55 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { baseURL } from '../priv';
 import CharacterDetailsCard from './CharacterDetailsCard';
 
 const CharacterDetails = ({ character }) => {
+  const [characterInfo, setCharacterInfo] = useState(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getCharacterInfo = async () => {
+      const res = await fetch(`${baseURL}&name=${params.name}`);
+      const data = await res.json();
+
+      setCharacterInfo(data.data.results[0]);
+    };
+    getCharacterInfo();
+  }, []);
+
   return (
     <>
-      {character && (
+      {characterInfo && (
         <div className='character-info'>
           <img
-            src={character.thumbnail.path + '/portrait_incredible.jpg'}
+            src={characterInfo.thumbnail.path + '/portrait_incredible.jpg'}
             alt='portrait_incredible'
             className='character-info__image'
           />
           <div className='character-info__story'>
-            <h1 className='character-info__name'>{character.name}</h1>
+            <h1 className='character-info__name'>{characterInfo.name}</h1>
             <p className='character-info__description'>
-              {character.description}
+              {characterInfo.description}
             </p>
             <div className='character-info__comics character-info__cards'>
-              {character.comics.items.map((comic, index) => (
+              {characterInfo.comics.items.map((comic, index) => (
                 <CharacterDetailsCard key={index} name={comic.name} />
               ))}
             </div>
             <div className='character-info__events character-info__cards'>
-              {character.events.items.map((comic, index) => (
-                <CharacterDetailsCard key={index} name={comic.name} />
+              {characterInfo.events.items.map((event, index) => (
+                <CharacterDetailsCard key={index} name={event.name} />
               ))}
             </div>
             <div className='character-info__series character-info__cards'>
-              {character.series.items.map((comic, index) => (
-                <CharacterDetailsCard key={index} name={comic.name} />
+              {characterInfo.series.items.map((serie, index) => (
+                <CharacterDetailsCard key={index} name={serie.name} />
               ))}
             </div>
             <div className='character-info__stories character-info__cards'>
-              {character.stories.items.map((storie, index) => (
-                <CharacterDetailsCard key={index} name={storie.name} />
+              {characterInfo.stories.items.map((story, index) => (
+                <CharacterDetailsCard key={index} name={story.name} />
               ))}
             </div>
           </div>
