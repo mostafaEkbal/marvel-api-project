a411f856906e13c92e6b5aad4748e8b25b8c0f99
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-09-15 01:45:05 +0200

    created create user functionality

--- marvel-app/src/components/SignIn.jsx
+++ marvel-app/src/components/SignIn.jsx
@@ -1,20 +1,45 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { UserAuth } from '../contexts/AuthContext';
+
 const SignIn = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+  const { createUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSumbit = async e => {
+    e.preventDefault();
+    setError('');
+
+    try {
+      await createUser(email, password);
+      navigate('/');
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
+  };
+
   return (
     <div className='max-w-[700px] mx-auto my-16 p-4'>
       <div>
         <h1 className='text-2xl font-bold py-2'>Sign in to your account</h1>
         <p className='py-2'>
           Don't have an account yet?{' '}
-          {/* <Link to='/signup' className='underline'>
-            Sign up.
-          </Link> */}
+          {
+            <Link to='/signup' className='underline'>
+              Sign up.
+            </Link>
+          }
         </p>
       </div>
-      <form /* onSubmit={handleSubmit} */>
+      <form onSubmit={handleSumbit}>
         <div className='flex flex-col py-2'>
           <label className='py-2 font-medium'>Email Address</label>
           <input
-            /* onChange={e => setEmail(e.target.value)} */
+            onChange={e => setEmail(e.target.value)}
             className='border p-3'
             type='email'
           />
@@ -22,7 +47,7 @@ const SignIn = () => {
         <div className='flex flex-col py-2'>
           <label className='py-2 font-medium'>Password</label>
           <input
-            /* onChange={e => setPassword(e.target.value)} */
+            onChange={e => setPassword(e.target.value)}
             className='border p-3'
             type='password'
           />
